f5b2680d52acd97f15d045af38aec8db230d089c
Jessica Anais Duarte Cabrera <duarte.jezzica@gmail.com>
2017-12-15 10:55:20 -0700

    fixed link

--- positive-sprinkle/src/components/Footer.js
+++ positive-sprinkle/src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <footer>
               <Row style={{ backgroundColor: "#eff1f4"}}>
                 <Col>
-                  <h3 style={{fontSize: 23, paddingTop:'6px'}}> Created by <a href="https://www.linkedin.com/in/jessica-anais-duarte-cabrera-540099148">Michael</a></h3>
+                  <h3 style={{fontSize: 23, paddingTop:'6px'}}> Created by <a href="https://www.linkedin.com/in/jessica-anais-duarte-cabrera-540099148">Jessica</a> 2017</h3>
                 </Col>
             </Row>
         </footer>
